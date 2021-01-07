019f2024550b2ddbd94c266df53d6c3cb303f2d1
Varx <varx@Varxs-Mac-mini.local>
2021-01-07 15:51:42 +0700

    update config, concat css

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,8 @@ const uglify = require('gulp-uglify');
 const useref = require('gulp-useref');
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
-const spritesmith = require('gulp.spritesmith');
-const buffer = require('vinyl-buffer');
+// const spritesmith = require('gulp.spritesmith');
+// const buffer = require('vinyl-buffer');
 const merge = require('merge-stream');
 const fonts = require('postcss-font-magician');
 const sourcemaps = require('gulp-sourcemaps');
@@ -22,6 +22,7 @@ purgecss = require('gulp-purgecss');
 // File Path
 const files = {
     scssPath: 'src/assets/scss/**/*.scss',
+    cssPath: 'src/assets/css/vendors/*.css',
     jsPath: 'src/assets/js/**/*.js',
     imgPath: 'src/assets/img/**/*',
 };
@@ -41,26 +42,50 @@ function browserSyncReload(done) {
     done();
 }
 
+// function scssTask() {
+//     const plugin = [
+//         autoprefixer(),
+//         postsvg(),
+//         cssnano(),
+//         fonts({
+//             foundries: 'bootstrap google',
+//         }),
+//     ];
+//     return gulp
+//         .src(files.scssPath)
+//         .pipe(sourcemaps.init())
+//         .pipe(sass())
+//         .on('error', sass.logError)
+//         .pipe(postcss(plugin))
+//         .pipe(sourcemaps.write())
+//         .pipe(gulp.dest('src/assets/css'))
+//         .pipe(browsersync.stream());
+// }
+
+//define default task
 function scssTask() {
-    const plugin = [
-        autoprefixer(),
-        postsvg(),
-        cssnano(),
-        fonts({
-            foundries: 'bootstrap google',
-        }),
-    ];
-    return gulp
-        .src(files.scssPath)
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(postcss(plugin))
-        .pipe(sourcemaps.write())
-        
+    var sassStream,
+        cssStream;
+
+    //compile sass
+    sassStream = gulp.src(files.scssPath)
+        .pipe(sass({
+            errLogToConsole: true
+        }));
+
+    //select additional css files
+    // cssStream = gulp.src(files.cssPath);
+    cssStream = gulp.src([
+        'node_modules/minibarjs/dist/minibar.min.css',
+    ]);
+
+    //merge the two streams and concatenate their contents into a single file
+    return merge(sassStream, cssStream)
+        .pipe(concat('style.css'))
         .pipe(gulp.dest('src/assets/css'))
         .pipe(browsersync.stream());
 }
+
 
 function jsTask() {
     return gulp
@@ -68,6 +93,7 @@ function jsTask() {
             'node_modules/@popperjs/core/dist/umd/popper.min.js',
             'node_modules/jquery/dist/jquery.min.js',
             'node_modules/bootstrap/dist/js/bootstrap.min.js',
+            'node_modules/minibarjs/dist/minibar.min.js'
             // 'node_modules/@fortawesome/fontawesome-free/js/all.js',
         ])
         .pipe(sourcemaps.init())
@@ -90,26 +116,26 @@ function fontTask() {
         .pipe(gulp.dest('dist/assets/fonts'));
 }
 
-function imageSprite() {
-    var spriteData = gulp.src(files.imgPath).pipe(
-        spritesmith({
-            imgName: 'sprite.png',
-            cssName: '_sprite.scss',
-            padding: 10,
-        })
-    );
+// function imageSprite() {
+//     var spriteData = gulp.src(files.imgPath).pipe(
+//         spritesmith({
+//             imgName: 'sprite.png',
+//             cssName: '_sprite.scss',
+//             padding: 10,
+//         })
+//     );
 
-    var imgStream = spriteData.img
-        .pipe(buffer())
-        .pipe(imagemin())
-        .pipe(gulp.dest('src/assets/img'));
+//     var imgStream = spriteData.img
+//         .pipe(buffer())
+//         .pipe(imagemin())
+//         .pipe(gulp.dest('src/assets/img'));
 
-    var cssStream = spriteData.css.pipe(
-        gulp.dest('src/assets/scss/components')
-    );
+//     var cssStream = spriteData.css.pipe(
+//         gulp.dest('src/assets/scss/components')
+//     );
 
-    return merge(imgStream, cssStream);
-}
+//     return merge(imgStream, cssStream);
+// }
 
 function js() {
     return gulp
@@ -120,7 +146,7 @@ function js() {
 
 function css() {
     return gulp
-        .src('src/assets/css/**/*.css')
+        .src('src/assets/css/style.css')
         .pipe(useref())
         .pipe(gulp.dest('dist/assets/css'));
 }
@@ -151,11 +177,12 @@ function cleanCSS() {
 }
 
 // Default
-exports.imageSprite = imageSprite;
+// exports.imageSprite = imageSprite;
 exports.default = gulp.series(gulp.parallel(scssTask, jsTask));
 
 // Production
-gulp.task('prod', gulp.parallel([cleanDist, css, js, imageTask, fontTask, userefTask, cleanCSS]));
+// gulp.task('prod', gulp.parallel([cleanDist, css, js, imageTask, fontTask, userefTask, cleanCSS]));
+gulp.task('prod', gulp.parallel([cleanDist, css, js, imageTask, fontTask, userefTask]));
 
 // Development
 gulp.task('dev', gulp.parallel(watchTask, browserSync, jsTask));
