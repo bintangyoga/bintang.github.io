de84664a372ef566a661fee8eb1b48b2d4d67437
Varx <varx@Varxs-Mac-mini.local>
2021-01-08 09:26:38 +0700

    update gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,12 @@ const uglify = require('gulp-uglify');
 const useref = require('gulp-useref');
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
-// const spritesmith = require('gulp.spritesmith');
-// const buffer = require('vinyl-buffer');
 const merge = require('merge-stream');
 const fonts = require('postcss-font-magician');
 const sourcemaps = require('gulp-sourcemaps');
 const browsersync = require('browser-sync').create();
 const del = require('del');
-purgecss = require('gulp-purgecss');
+const purgecss = require('gulp-purgecss');
 
 // File Path
 const files = {
@@ -42,44 +40,31 @@ function browserSyncReload(done) {
     done();
 }
 
-// function scssTask() {
-//     const plugin = [
-//         autoprefixer(),
-//         postsvg(),
-//         cssnano(),
-//         fonts({
-//             foundries: 'bootstrap google',
-//         }),
-//     ];
-//     return gulp
-//         .src(files.scssPath)
-//         .pipe(sourcemaps.init())
-//         .pipe(sass())
-//         .on('error', sass.logError)
-//         .pipe(postcss(plugin))
-//         .pipe(sourcemaps.write())
-//         .pipe(gulp.dest('src/assets/css'))
-//         .pipe(browsersync.stream());
-// }
-
-//define default task
 function scssTask() {
+    const plugin = [
+        autoprefixer(),
+        postsvg(),
+        cssnano(),
+        fonts({
+            foundries: 'bootstrap google',
+        }),
+    ];
+
     var sassStream,
         cssStream;
 
-    //compile sass
     sassStream = gulp.src(files.scssPath)
-        .pipe(sass({
-            errLogToConsole: true
-        }));
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(postcss(plugin))
+        .pipe(sourcemaps.write())
 
-    //select additional css files
-    // cssStream = gulp.src(files.cssPath);
     cssStream = gulp.src([
         'node_modules/minibarjs/dist/minibar.min.css',
+        'node_modules/@splidejs/splide/dist/css/splide.min.css',
     ]);
 
-    //merge the two streams and concatenate their contents into a single file
     return merge(sassStream, cssStream)
         .pipe(concat('style.css'))
         .pipe(gulp.dest('src/assets/css'))
@@ -93,8 +78,8 @@ function jsTask() {
             'node_modules/@popperjs/core/dist/umd/popper.min.js',
             'node_modules/jquery/dist/jquery.min.js',
             'node_modules/bootstrap/dist/js/bootstrap.min.js',
-            'node_modules/minibarjs/dist/minibar.min.js'
-            // 'node_modules/@fortawesome/fontawesome-free/js/all.js',
+            'node_modules/minibarjs/dist/minibar.min.js',
+            'node_modules/@splidejs/splide/dist/js/splide.min.js'
         ])
         .pipe(sourcemaps.init())
         .pipe(concat('script.js'))
@@ -115,27 +100,6 @@ function fontTask() {
         .src('src/assets/fonts/**/*')
         .pipe(gulp.dest('dist/assets/fonts'));
 }
-
-// function imageSprite() {
-//     var spriteData = gulp.src(files.imgPath).pipe(
-//         spritesmith({
-//             imgName: 'sprite.png',
-//             cssName: '_sprite.scss',
-//             padding: 10,
-//         })
-//     );
-
-//     var imgStream = spriteData.img
-//         .pipe(buffer())
-//         .pipe(imagemin())
-//         .pipe(gulp.dest('src/assets/img'));
-
-//     var cssStream = spriteData.css.pipe(
-//         gulp.dest('src/assets/scss/components')
-//     );
-
-//     return merge(imgStream, cssStream);
-// }
 
 function js() {
     return gulp
@@ -176,12 +140,10 @@ function cleanCSS() {
     .pipe(gulp.dest('dist/'))
 }
 
-// Default
-// exports.imageSprite = imageSprite;
+// On Save
 exports.default = gulp.series(gulp.parallel(scssTask, jsTask));
 
 // Production
-// gulp.task('prod', gulp.parallel([cleanDist, css, js, imageTask, fontTask, userefTask, cleanCSS]));
 gulp.task('prod', gulp.parallel([cleanDist, css, js, imageTask, fontTask, userefTask]));
 
 // Development
